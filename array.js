4758c58a4fa94884515c53811800dc5ec2d7ccaf
abinaya0708 <aabinaya2001.com>
2023-11-05 17:53:36 +0530

    array

---
+++ array.js
@@ -0,0 +1,29 @@
+document.write("ascending order of an array elements"+"<br>");
+document.write("Given array:[1,5,3,-5,7,8,-1]"+"<br>");
+
+
+
+
+
+let a=[1,5,3,-5,7,8,-1];
+
+let b=[];
+temp=0;
+
+for(i=0;i<a.length;i++){
+	for(j=i+1;j<a.length;j++){
+		if(a[i]>a[j]){
+			temp=a[j];
+			a[j]=a[i];
+			a[i]=temp;
+		}
+	}
+	
+	
+b.push(a[i]);
+
+}
+console.log(b);
+
+
+document.write("Output:"+b);
